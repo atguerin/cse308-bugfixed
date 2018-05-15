cfc9d91554437bc4ad055ef06f12b494048e3b5b
Adam <atguerin@cs.stonybrook.edu>
2018-05-15 17:54:36 -0400

    BUG FIXES DO NOT REVERT 2

--- src/main/java/com/maple/cse308/repository/ReportMovieUserReviewRepository.java
+++ src/main/java/com/maple/cse308/repository/ReportMovieUserReviewRepository.java
@@ -2,9 +2,11 @@ package com.maple.cse308.repository;
 
 import com.maple.cse308.entity.ReportMovieUserReview;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReportMovieUserReviewRepository extends CrudRepository<ReportMovieUserReview, Integer>{
     ReportMovieUserReview save(ReportMovieUserReview reportMovieUserReview);
     List<ReportMovieUserReview> findAll();
--- src/main/java/com/maple/cse308/repository/ReportTvCriticReviewRepository.java
+++ src/main/java/com/maple/cse308/repository/ReportTvCriticReviewRepository.java
@@ -2,9 +2,11 @@ package com.maple.cse308.repository;
 
 import com.maple.cse308.entity.ReportTvCriticReview;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReportTvCriticReviewRepository extends CrudRepository<ReportTvCriticReview, Integer>{
     ReportTvCriticReview save(ReportTvCriticReview reportTvCriticReview);
     List<ReportTvCriticReview> findAll();
